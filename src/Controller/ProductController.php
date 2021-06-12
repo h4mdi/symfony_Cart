45bd3b71f9120ccb3b2437ec46012a83ff9c6cc0
HAMDI BOUROUNIA <h4mdy1234@gmail.com>
2021-06-12 19:43:32 +0100

    make it better

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/", name="product_index")
+     */
+    public function index(ProductRepository $productRepository)
+    {
+        return $this->render('home/index2.html.twig', [
+            'products' => $productRepository->findAll()
+        ]);
+    }
+
+    
+}
